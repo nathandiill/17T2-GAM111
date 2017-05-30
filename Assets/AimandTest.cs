17de80f19b2ee07e479e340ea487cb2515d11570
Nathan Diill <Nathan Diill>
2017-05-30 11:52:12 +1000

    After Week 1.

---
+++ Assets/AimandTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimandTest : MonoBehaviour {
+    
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        var mousePos = Input.mousePosition;
+        var camRay = Camera.main.ScreenPointToRay(mousePos);
+        var playField = new Plane(Vector3.back, Vector3.zero);
+
+        float outDist = 0;
+        if (playField.Raycast(camRay, out outDist))
+        {
+            var hitPos = camRay.GetPoint(outDist);
+
+            Debug.DrawLine(transform.position, hitPos, Color.yellow);
+
+            var dif = hitPos - transform.position;
+
+            // Give that to a new bullet as which way it needs to travel
+            var bulletDirection = dif.normalized;
+        }
+	}
+}
